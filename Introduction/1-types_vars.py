c89847c51f2af67fb38de14679f64e584b0ad79e
Dev B <devb@bu.edu>
2023-01-15 21:39:02 -0500

    Added intro folder; need to expand parts 2-4

---
+++ Introduction/1-types_vars.py
@@ -0,0 +1,20 @@
+# A small program to demonstrate the differences between types
+
+
+print(type(100))
+
+print(type(100.1))
+
+print(type("100"))
+
+print(type("100.1"))
+
+print(type(True))
+
+temporary_integer = 100
+
+temporary_float = 100.110101010101010101010
+
+temporary_string = "Hello World!"
+
+print(temporary_string)
