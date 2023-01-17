905c992d530258e178fa066f249b6c717c9d1b4d
Dev B <devb@bu.edu>
2023-01-16 21:30:38 -0500

    Updated file structure

---
+++ Introduction/Week 1/1-math.py
@@ -0,0 +1,17 @@
+# statments to demonstrate the usage of different mathematical operators
+
+print("3 + 2 = ", 3 + 2) 
+
+print("3 - 2 =", 3 - 2)
+
+print("3 * 2 =", 3 * 2) 
+
+print("3 ** 2 =", 3 ** 2) 
+
+print("3/2 = ", 3/2)
+
+print("3//2 = ", 3//2) 
+
+print("3%2 = ", 3%2) 
+
+print("2 * (3 + 4) = ", 2 * (3 + 4))
---
+++ Introduction/Week 1/2-types_vars.py
@@ -0,0 +1,11 @@
+# A small program to demonstrate the differences between types
+
+print(type(100))
+
+print(type(100.1))
+
+temporary_integer = 100
+
+temporary_float = 100.5
+
+print(temporary_integer - temporary_float)
