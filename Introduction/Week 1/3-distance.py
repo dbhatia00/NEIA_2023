87261406436892d316f18f4a257938b7f233f54b
Dev B <devb@bu.edu>
2023-01-22 16:18:47 -0500

    Did in class example

--- Introduction/Week 1/3-distance.py
+++ Introduction/Week 1/3-distance.py
@@ -1,3 +1,16 @@
 # A program to calculate the distance between two points
 
 # Assume p1 = (10, 10) and p2 = (0, 0)
+
+import math
+
+x1 = 3
+x2 = 0
+
+y1 = 4
+y2 = 0
+
+x_block = (x2 - x1) ** 2
+y_block = (y2 - y1) ** 2
+
+print(math.sqrt(x_block + y_block))
