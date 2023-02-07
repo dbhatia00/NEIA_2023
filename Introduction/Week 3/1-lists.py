aae2c22a2afe5a5219bb747d6c5c21603d49a429
Dev B <devb@bu.edu>
2023-02-07 17:51:34 -0500

    Fixed wk 3 plan

--- Introduction/Week 3/1-lists.py
+++ Introduction/Week 3/1-lists.py
@@ -1,1 +1,17 @@
 # write a program that requests five grades as input, displays the average - BUT, only after dropping the two lowest grades
+
+grades = []
+grade = float(input('Enter your first grade:'))
+
+
+grade = float(input('Enter your grade:'))
+
+
+grade = float(input('Enter your grade:'))
+
+
+grade = float(input('Enter your grade:'))
+
+
+grade = float(input('Enter your grade:'))
+
---
+++ Introduction/Week 3/3-triangle.py
@@ -0,0 +1,16 @@
+# A program to determine which triangle is largest
+# Using lists and conditionals!
+
+import math
+
+points = []
+
+x = float(input("Enter p1's x: "))
+y = float(input("Enter p1's y: "))
+
+x = float(input("Enter p2's x: "))
+y = float(input("Enter p2's y: "))
+
+x = float(input("Enter p3's x: "))
+y = float(input("Enter p3's y: "))
+
